7316d67a18b65ed8a55c7da7e51e8701d20ef812
javielsitorico <zx21student030@colegiovalledelmiro.es>
2023-02-09 13:59:21 +0100

    Formulario de registro a tiempo de ejecucion añadido (comentario)

--- registration/views.py
+++ registration/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.views.generic import CreateView
 from django.urls import reverse_lazy
+from django import forms
 
 class RegistroView(CreateView):
      form_class = UserCreationForm
@@ -9,3 +10,11 @@ class RegistroView(CreateView):
      
      def get_success_url(self) -> str:
           return reverse_lazy('login') + '?registrado'
+     
+     # Asi es como se generarian formularios en tiempo de ejecucion, pudiendo cmodificarlos como queramos
+     # def get_form(self, form_class: None):
+     #      form = super(RegistroView, self).get_form()
+     #      form.fields['_username'].widget = forms.textInput(aatrs={'placeholder':'...', 'style':'background-color: red'})
+     #      form.fields['_password1'].widget = forms.textInput(aatrs={'placeholder':'...', 'style':'background-color: red'})  
+     #      form.fields['_password2'].widget = forms.textInput(aatrs={'placeholder':'...', 'style':'background-color: red'})  
+     #      return form
